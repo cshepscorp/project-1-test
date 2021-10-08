c50106372537cc0006aff6f4c0cfa1d289458934
cshepscorp <sheppard.christy@gmail.com>
2021-10-08 10:51:43 -0400

    latest updates to group project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,28 @@
-// Create a function called `myFunction()`
+// here is my little note for Will
 // YOUR CODE HERE
 var searchTerm = document.querySelector("#searchTerm");
+var searchTerm2 = document.querySelector("#searchTerm2");
+
+var eventsButton = document.querySelector("#events-button");
+var eventsSearchResultsEl = document.querySelector("#events-search-results1");
+var eventsSearchResultsEl2 = document.querySelector("#events-search-results2");
+var eventsSearchResultsEl3 = document.querySelector("#events-search-results3");
+
 
 // API key ticketmaster
 var tmApi = '&apikey=2MALjZsA5tAXCU1xKvJPNTzJVAsqk24J';
 
-// API stuff for Yelp
-// Client ID
-// Bq0WKZfzysX-Slot-wMLOg
-
-// API Key
-// mi3KrWTTIR6u6MzcBQIouugyJle-VR6pDa8Um5HOTaq8MbS2em5Y3ulktm9qsQbLmnc5ZfOB2hume657ENCZsdL2aJLXsq4Q136tr6BtkRZmVH49RgXLZv351cNdYXYx
-
-var yelpApi = 'mi3KrWTTIR6u6MzcBQIouugyJle-VR6pDa8Um5HOTaq8MbS2em5Y3ulktm9qsQbLmnc5ZfOB2hume657ENCZsdL2aJLXsq4Q136tr6BtkRZmVH49RgXLZv351cNdYXYx';
-var yelpdClientId = 'Bq0WKZfzysX-Slot-wMLOg';
-
-// YOUR CODE HERE
+var addHideClass = function() {
+  console.log('events button was clicked');
+  eventsSearchResultsEl.classList.remove('hide');
+  eventsSearchResultsEl2.classList.remove('hide');
+  eventsSearchResultsEl3.classList.remove('hide');
+};
 var loadEventsByCity = function() {
   // var rating = document.getElementById("rating").value;
   var theirSearch = searchTerm.value.trim();
-  // var apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&q=' + theirSearch + '&rating=' + rating;
-  var brewApiUrl = 'https://api.openbrewerydb.org/breweries/search?query=' + theirSearch;
-
-  fetch(brewApiUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        // brewContainer.innerHTML = '';
-        // Create a variable that will select the <div> where the GIF will be displayed
-        var brewHeadline = document.createElement('h3');
-        brewHeadline.textContent = 'Showing all breweries in ' + searchTerm.value;
-        
-        for(var i = 0; i < response.length; i++) {
-          // console.log(response[i]);
-          var brewContainer = document.querySelector("#response-container");
-          
-          var brewSearchListItem = document.createElement('li');
-          var brewSearchReturn = document.createElement('a');
-          brewSearchReturn.setAttribute('href', response[i].website_url);
-          brewSearchReturn.setAttribute('type', 'submit');
-          brewSearchReturn.setAttribute('target', '_blank');
-          brewSearchReturn.textContent = response[i].name;
-          console.log(brewSearchReturn + 'is bse');
-          brewContainer.append(brewSearchListItem);
-          brewSearchListItem.append(brewSearchReturn);
-
-          brewContainer.append(brewSearchReturn);
-          searchTerm.value = '';
-        };
-        brewContainer.prepend(brewHeadline);
-      });
-
+  eventsSearchResultsEl.classList.add("show")
+ 
       var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?sort=date,asc&city='
       + theirSearch + tmApi;
 
@@ -60,63 +31,152 @@ var loadEventsByCity = function() {
             return response.json();
           })
           .then(function(response) {
-            // Create a variable that will select the <div> where the GIF will be displayed
-            var univHeadline = document.createElement('h3');
-            univHeadline.textContent = 'Showing all events in ' + searchTerm.value;
-            
+
             for(var i = 0; i < response._embedded.events.length; i++) {
-              var univContainer = document.querySelector("#event-container");
+              var univContainer = document.querySelector("#event-response-container");
 
-              var univSearchReturnList = document.createElement('li');
-              var univSearchReturn = document.createElement('a');
-              univSearchReturn.setAttribute('href', response._embedded.events[i].url);
-              univSearchReturn.setAttribute('type', 'submit');
-              univSearchReturn.setAttribute('target', '_blank');
-              univSearchReturn.textContent = response._embedded.events[i].name + ' (' + response._embedded.events[i].dates.start.localDate + ')';
+              var univSearchReturnListContainer = document.createElement('div');
+              univSearchReturnListContainer.classList = "col s12 m6";
+              univContainer.append(univSearchReturnListContainer);
+
+              var univSearchReturnList = document.createElement('div');
+              univSearchReturnList.classList = "card";
+              univSearchReturnList.style.height = '200px';
+              univSearchReturnList.style.maxHeight = '210px';
+              univSearchReturnListContainer.append(univSearchReturnList);
+
+              var univSearchReturnCardDiv = document.createElement('div');
+              univSearchReturnCardDiv.classList = "card-list";
+              univSearchReturnList.append(univSearchReturnCardDiv);
+
+              var univSearchReturnCardContentDiv = document.createElement('div');
+              univSearchReturnCardContentDiv.classList = "card-content";
+              univSearchReturnCardContentDiv.style.backgroundImage = `url('${response._embedded.events[i].images[3].url}')`;
+              univSearchReturnCardContentDiv.style.backgroundRepeat = 'none';
+              univSearchReturnCardContentDiv.style.backgroundPosition = 'center center';
               
-              // Append to the <div>
-              univContainer.append(univSearchReturnList);
-              univSearchReturnList.append(univSearchReturn);
+              univSearchReturnList.append(univSearchReturnCardContentDiv);
+
+              var univSearchReturnCardContentP = document.createElement('p');
+              univSearchReturnCardContentP.classList = "card-content";
+              univSearchReturnCardContentP.innerHTML = response._embedded.events[i].name + `<br />` 
+              + `<span class="font-date">` + response._embedded.events[i].dates.start.localDate + `</span>` 
+              + `<br />` + `<span class="font-date">` + response._embedded.events[i]._embedded.venues[0].name + `</span>`;
+              univSearchReturnCardContentDiv.append(univSearchReturnCardContentP);
+
+              var univSearchReturnCardActionDiv = document.createElement('div');
+              univSearchReturnCardActionDiv.classList = "card-action";
+              univSearchReturnList.append(univSearchReturnCardActionDiv);
+
+              var univSearchReturnCardActionLink = document.createElement('a');
+              // univSearchReturnCardActionLink.classList = "card-content";
+              univSearchReturnCardActionLink.innerHTML = `<a href="${response._embedded.events[i].url}" target="_blank">Event Link</a>`;
+              univSearchReturnCardActionDiv.append(univSearchReturnCardActionLink);
+
               searchTerm.value = '';
             };
-            univContainer.prepend(univHeadline);
+
           });
 
-          var yelpApiUrl = 'https://api.yelp.com/v3/businesses/search?location=' + theirSearch;
-          let h = new Headers();
-          h.append('Accept', 'application/json');
+          var brewApiUrl = 'https://api.openbrewerydb.org/breweries/search?query=' + theirSearch;
+  
+  fetch(brewApiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        
+        for(var i = 0; i < 10; i++) {
+          // console.log(response[i]);
+          var brewContainer = document.querySelector("#brewery-response-container-events");
+          
+          var brewSearchListItem = document.createElement('div');
+          brewSearchListItem.classList ="col";
+          brewContainer.append(brewSearchListItem);
+          
+          var brewSearchListItemCardDiv = document.createElement('div');
+          brewSearchListItemCardDiv.classList = "brew-list grey lighten-4 card";
+          brewSearchListItemCardDiv.style.height = '200px';
+          brewSearchListItemCardDiv.style.width = '200px'
+          brewSearchListItem.append(brewSearchListItemCardDiv);
 
-          let req = new Request(yelpApiUrl, {
-            method: 'GET',
-            headers: h,
-            mode: 'no-cors'
-          })
+          var brewSearchListItemCardSpan = document.createElement('span');
+          brewSearchListItemCardSpan.classList = "brewery-title";
+          brewSearchListItemCardSpan.textContent = response[i].name;
+          brewSearchListItemCardDiv.append(brewSearchListItemCardSpan);
 
-      fetch(req) 
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(response) {
-            // Create a variable that will select the <div> where the GIF will be displayed
-            var yelpHeadline = document.createElement('h3');
-            yelpHeadline.textContent = 'Showing all events in ' + searchTerm.value;
-            
-            for(var i = 0; i < response.businesses.length; i++) {
-              var yelpContainer = document.querySelector("#event-container");
+          var brewSearchListItemCardP = document.createElement('li');
+          brewSearchListItemCardP.textContent = response[i].street
+          brewSearchListItemCardDiv.append(brewSearchListItemCardP);
 
-              var yelpSearchReturnList = document.createElement('li');
-              var yelpSearchReturn = document.createElement('a');
-              yelpSearchReturn.setAttribute('href', response.businesses[i].url);
-              yelpSearchReturn.setAttribute('type', 'submit');
-              yelpSearchReturn.setAttribute('target', '_blank');
-              yelpSearchReturn.textContent = response.businesses[i].name + ' (Rating:' + response.businesses[i].rating + ')';
-              
-              // Append to the <div>
-              univContainer.append(yelpSearchReturnList);
-              yelpSearchReturnList.append(yelpSearchReturn);
-              searchTerm.value = '';
-            };
-            yelpContainer.prepend(yelpHeadline);
-          });
+          var brewSearchListItemCardP2 = document.createElement('li');
+          brewSearchListItemCardP2.textContent = response[i].phone;
+          brewSearchListItemCardP2.style.marginBottom = '20px';
+          brewSearchListItemCardDiv.append(brewSearchListItemCardP2);
+
+          var brewSearchReturnLink = document.createElement('button');
+          brewSearchReturnLink.classList = "btn";
+          brewSearchReturnLink.innerHTML = `<a href="${response[i].website_url}" target="_blank" style="font-size:12px; color: white">Visit Website</a>`;
+          brewSearchListItemCardDiv.append(brewSearchReturnLink);
+
+          searchTerm.value = '';
+        };
+
+      });
         
 };
+
+var loadBreweriesByCity = function() {
+  // var rating = document.getElementById("rating").value;
+  var theirSearch = searchTerm2.value.trim();
+ 
+  var brewApiUrl = 'https://api.openbrewerydb.org/breweries/search?query=' + theirSearch;
+  
+  fetch(brewApiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        
+        for(var i = 0; i < 12; i++) {
+          // console.log(response[i]);
+          var brewContainer = document.querySelector("#brewery-response-container");
+          
+          var brewSearchListItem = document.createElement('div');
+          brewSearchListItem.classList ="col s12 m4";
+          brewContainer.append(brewSearchListItem);
+          
+          var brewSearchListItemCardDiv = document.createElement('div');
+          brewSearchListItemCardDiv.classList = "brew-list grey lighten-4 card";
+          brewSearchListItemCardDiv.style.height = '180px';
+          brewSearchListItem.append(brewSearchListItemCardDiv);
+
+          var brewSearchListItemCardSpan = document.createElement('span');
+          brewSearchListItemCardSpan.classList = "brewery-title";
+          brewSearchListItemCardSpan.textContent = response[i].name;
+          brewSearchListItemCardDiv.append(brewSearchListItemCardSpan);
+
+          var brewSearchListItemCardP = document.createElement('li');
+          brewSearchListItemCardP.textContent = response[i].street;
+          brewSearchListItemCardDiv.append(brewSearchListItemCardP);
+
+          var brewSearchListItemCardP2 = document.createElement('li');
+          brewSearchListItemCardP2.textContent = response[i].phone;
+          brewSearchListItemCardP2.style.marginBottom = '10px';
+          brewSearchListItemCardDiv.append(brewSearchListItemCardP2);
+
+          var brewSearchReturnLink = document.createElement('button');
+          brewSearchReturnLink.classList = "btn";
+          brewSearchReturnLink.innerHTML = `<a href="${response[i].website_url}" target="_blank" style="font-size:12px; color: white">Visit Website</a>`;
+          brewSearchListItemCardDiv.append(brewSearchReturnLink);
+
+          searchTerm.value = '';
+        };
+
+      });
+        
+};
+
+eventsButton.addEventListener("click", addHideClass);
+
+
